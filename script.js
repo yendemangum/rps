691687c67370b7c5a35adf5824bed0d04de19a96
Yende Mangum <yendemangum@gmail.com>
2024-10-02 13:19:09 -0400

    Add buttoned UI to rps game
    Game displays winner after 5 rounds. If tie, game continues uneven score
    at a multiple  of 5.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const container = document.querySelector("#container")
+
+const rock = document.createElement("button")
+rock.setAttribute("id", "rock")
+rock.textContent = "rock"
+const paper = document.createElement("button")
+paper.setAttribute("id", "paper")
+paper.textContent = "paper"
+const scissors = document.createElement("button")
+scissors.setAttribute("id", "scissors")
+scissors.textContent = "scissors"
+
+container.appendChild(rock)
+container.appendChild(paper)
+container.appendChild(scissors)
+
+
 function getComputerChoice() {
     let choice = Math.ceil(Math.random()*3);
     if (choice == 1) {
@@ -11,34 +28,65 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Choose rock, paper, or scissors").toLowerCase()
-    while (choice != "rock" && choice != "paper" && choice != "scissors") {
-        choice = prompt("Type rock, paper, or scissors")
-    }
-    return choice;
-}
+
 
 let humanScore = 0;
 let computerScore = 0;
 
+function getHumanChoice() {
+    let container = document.querySelector("#container")
+    let playerSelection = ""
+    container.addEventListener("click", (event) => {
+        let target = event.target;
+        
+        if (target.id == "rock") {
+            playerSelection = "rock"
+        }
+        else if (target.id == "paper") {
+            playerSelection = "paper"
+        }
+        else if (target.id == "scissors") {
+            playerSelection = "scissors"
+         }
+   playRound(playerSelection, getComputerChoice())
+ } )  
+}
+
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+ 
+const results = document.createElement("div")
+    results.setAttribute("id", "results")
+    
+    if ((humanScore + computerScore) % 5 == 0) {
+        if (humanScore > computerScore) {
+            results.textContent = 'You win by a score of ' + humanScore + " to " + computerScore + '!'
+            container.appendChild(results)
+            return}
+        if (computerScore > humanScore) {
+            results.textContent = 'You lose by a score of ' + humanScore + " to " + computerScore + '!'
+            container.appendChild(results)
+            return;}
+
+    }
+
+    
 
     if (humanChoice == computerChoice) {
         humanScore += .5
         computerScore += .5
-        console.log("Tie!")
+        const output = document.createTextNode("Tie!")
+        results.appendChild(output)
     }
 
     else if (humanChoice == "rock") {
         if (computerChoice == "paper") {
-            console.log("You Lose! Paper covers rock!")
+            const output = document.createTextNode("You Lose! Paper covers rock!")
+        results.appendChild(output)
             computerScore++
         }
         if (computerChoice == "scissors") {
-            console.log("You Win! Rock crushes scissors!")
+            const output = document.createTextNode("You Win! Rock crushes scissors!")
+            results.appendChild(output)
             humanScore++
          }
         
@@ -46,11 +94,13 @@ function playRound(humanChoice, computerChoice) {
 
     else if (humanChoice == "paper") {
         if (computerChoice == "rock") {
-            console.log("You Win! Paper covers rock!")
+            const output = document.createTextNode("You Win! Paper covers rock!")
+            results.appendChild(output)
             humanScore++
         }
         if (computerChoice == "scissors") {
-            console.log("You Lose! Scissors cuts paper!")
+            const output = document.createTextNode("You Lose! Scissors cuts paper!")
+            results.appendChild(output)
             computerScore++
          }
         
@@ -58,32 +108,26 @@ function playRound(humanChoice, computerChoice) {
 
     else if (humanChoice == "scissors") {
         if (computerChoice == "rock") {
-            console.log("You Lose! Rock crushes scissors!")
+            const output = document.createTextNode("You Lose! Rock crushes scissors!")
+            results.appendChild(output)
             computerScore++
         }
         if (computerChoice == "paper") {
-            console.log("You Win! Scissors cuts paper!")
+            const output = document.createTextNode("You Win! Scissors cuts paper!")
+            results.appendChild(output)
             humanScore++
          }
         
     }
 
-    console.log("Your score: " + humanScore);
-    console.log("Computer score: " + computerScore);
-    playAgain();
+    const output2 = document.createTextNode (" Your score: " + humanScore + ", " + "Computer score: " + computerScore);
+    results.appendChild(output2)
+    container.appendChild(results)
+
 }
 
-playRound();
+getHumanChoice();
 
-function playAgain() {
-    let choice = prompt("Play again?").toLowerCase()
-    if (choice == "yes"){
-        playRound();
-    }
-    else if (choice == "no"){
-        return;
-    }
-    else {
-        playAgain()
-    }
-}
+
+
+
